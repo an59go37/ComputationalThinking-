82d6417ecfe4f156028e490b735ee5fd2d419149
Angelina Gorgovski <an57go39@isdedu.de>
2015-09-03 15:31:55 +0200

    ompleted the programe

--- VowelConsonant.py
+++ VowelConsonant.py
@@ -1,1 +1,29 @@
 print("Hello World")
+eo o
+
+hll wrld
+
+userInput \= ("Enter your username")
+
+print (userinput)
+
+vowels="AEIOUaeiou"
+
+display vowels= ""
+
+displayConsonants=""
+
+for letter in userInput:
+    print ("letter:" + letter")
+
+    if letter in vowels:
+        print (letter)
+
+        displayVowels= displayVowels + letter
+
+    print ("vowels:" + displayVowels")
+
+else:
+    display displayConsonants
+    print ("consonants: " + displayConsonanats)
+    
